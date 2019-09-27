d4337ee95d3ad8569befad0413aec990589a6f0e
IallOus <cinthya_cyc@outlook.com>
2019-09-27 11:35:55 -0300

    primer commit para crear rama master

---
+++ EmpleadosP01Mod3/src/java/cl/duoc/Logica.java
@@ -0,0 +1,12 @@
+
+package cl.duoc;
+
+import java.util.List;
+
+public class Logica {
+    
+    public static List<Empleado> Listar() {
+        BussEmpleado obj=new BussEmpleado();
+        return obj.Listar();
+    }
+}
